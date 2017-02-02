bfc960321cb61599c5dddea0e9c0196971281cb7
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-02-02 17:26:52 +0100

    Update string.js
    add spacing

--- string.js
+++ string.js
@@ -1,6 +1,6 @@
 //ex. 8.8
-var text='Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach';
-var text2='Zielone słonie';
-text2=text2.toUpperCase();
-text=text.replace('Papugi',text2);
-console.log(text.substr(0,text.lenght/2));
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach';
+var text2 = 'Zielone słonie';
+text2 = text2.toUpperCase();
+text = text.replace('Papugi', text2);
+console.log(text.substr(0, text.lenght / 2));
